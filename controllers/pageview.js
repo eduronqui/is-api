a0023bb033fc267fb58e9933341d37caa74a7c1d
Eduardo Ronqui <edu.ronqui@gmail.com>
2015-11-28 14:43:25 -0200

    Added support to MySql

--- controllers/pageview.js
+++ controllers/pageview.js
@@ -1,14 +1,26 @@
+var mysql = require('../helpers/mysql.js');
 
 var getPageView = function (req, res) {
-	res.status(200).send(req.params.id);
+	mysql.executeQuery('select * from t_dummy', function (data) {
+		
+		for (var i = 0; i < data.length; i++){
+			var row = data[i];
+			console.log('chave: ' + row.chave);
+			console.log('valor: ' + row.valor);
+		}
+		
+		res.status(200).send(req.params.id);
+	});
 };
 
 var addHit = function (req, res) {
 	
 	var pageview = {
-		userId: req.body.usr,
-		productId: req.body.pid
+		chave: req.body.chave,
+		valor: req.body.valor
 	};
+	
+	mysql.executeInsert('t_dummy', pageview);
 	
 	res.status(201)
 		.send(JSON.stringify(pageview));
---
+++ helpers/mysql.js
@@ -0,0 +1,52 @@
+var db = require('mysql');
+var dbConfig = require('../package').config.db;
+
+var createConnection = function () {
+	return db.createConnection({
+		host: dbConfig.host,
+		port: dbConfig.port,
+		user: dbConfig.user,
+		password: dbConfig.password,
+		database: dbConfig.database
+	});
+};
+
+var mysql = {
+	
+	executeQuery: function (query, callback) {
+		
+		var connection = createConnection();
+		connection.query(query, function (err, rows) {
+			if (err)
+				console.log('There was an error! :( ...');
+			
+			if(callback)	
+				callback(rows);
+		});	
+		
+		connection.end(function (err) {
+			if (err)
+				console.log('There was an error closing db connection.');
+		});
+	},
+	
+	executeInsert: function (table, jdata, callback) {
+		
+		var sql = 'insert into {table} set ?'.replace('{table}', table);
+		
+		var connection = createConnection();
+		connection.query(sql, jdata, function (err, result) {
+			if (err)
+				console.log('There was an error! :( ...');
+
+			if (callback)	
+				callback(result);
+		});
+		
+		connection.end(function (err) {
+			if (err)
+				console.log('There was an error closing db connection.');
+		});
+	}
+};
+module.exports = mysql;

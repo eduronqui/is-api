2930ae0ada396200f38f2183fd53d90c4124da43
Juliano Nicomedes <julianonic@gmail.com>
2015-11-29 01:10:52 -0200

    R Scripts

--- rscripts/rscript.R
+++ rscripts/rscript.R
@@ -1,25 +1,54 @@
 #install.packages("RMySQL");
+#install.packages("jsonlite");
+#install.packages("data.table");
 library(RMySQL); #Carregando a Lib RMySQL
 library(dplyr);
 library(arules);
-mydb = dbConnect(MySQL(), user='hackathon', password='hackainm', dbname='hackathon-commerce', host='hackathon.cgzz1hagylvs.us-east-1.rds.amazonaws.com');
+library(jsonlite);
+library(data.table);
+#mydb = dbConnect(MySQL(), user='hackathon', password='hackainm', dbname='hackathon-commerce', host='hackathon.cgzz1hagylvs.us-east-1.rds.amazonaws.com');
 
-rs = dbSendQuery(mydb, "Select
-t1.id_order,
-t4.product_name
-from ps_orders t1
-left outer join ps_order_detail t4 on t1.id_order = t4.id_order");
+mydb = dbConnect(MySQL(), user='root', password='1234', dbname='oc_database', host='192.168.99.100', port=32779);
+
+
+
+#rs = dbSendQuery(mydb, "Select
+#t1.id_order,
+#t4.product_name
+#from ps_orders t1
+#left outer join ps_order_detail t4 on t1.id_order = t4.id_order");
+
+rs = dbSendQuery(mydb, "SELECT * FROM oc_transactions where transaction_type = 'P'");
+
 
 my_data <- fetch(rs, n=-1);
 #TS <- my_data %>% group_by(id_order) %>% summarise(vector=paste(product_name, collapse=","));
 
-write.csv(my_data, file = "data.csv", row.names=FALSE)
+write.csv(my_data, file = "data.csv")
 
-tr <- read.transactions("data.csv", format = "single",sep ="," , cols = c(1,2))
 
-m1 <- apriori(tr, parameter = list(support=0.007, confidence= 0.15, minlen = 2))
+tr <- read.transactions("data.csv", format = "single", sep ="," , cols = c("transaction_id","transaction_items")); #Alterar Parâmetros
 
+#tr <- read.transactions("data.csv", format = "basket", sep ="," , cols = 6 );
+
+#m1 <- apriori(tr, parameter = list(support=0.007, confidence= 0.15, minlen = 2)); #Alterar Parâmetros
+m1 <- apriori(tr, parameter = list(support=0.001, confidence= 0.20))
 
 inspect(m1)
+
+dfrules <- as(m1, "data.frame")
+
+SplittedDF <- data.frame(do.call('rbind', strsplit(as.character(dfrules$rules),'=>', fixed=TRUE)))
+dfrules$ruleif <- SplittedDF["X1"]
+dfrules$rulethen <- SplittedDF["X2"]
+
+
+
+
+cat(toJSON(dfrules));
+dbDisconnect(mydb);
+
+
+
 
 
--- sqlscripts/database.sql
+++ sqlscripts/database.sql
@@ -4,7 +4,19 @@ CREATE TABLE `oc_database`.`oc_transactions` (
   `id` INT NOT NULL COMMENT '',
   `client_id` INT NULL COMMENT '',
   `store_id` INT NULL COMMENT '',
+  `transaction_id` INT NULL COMMENT '',
   `transaction_type` VARCHAR(2) NULL COMMENT '',
   `transaction_items` LONGTEXT NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '');
 
+
+  CREATE TABLE `oc_database`.`oc_rules` (
+  `id_oc_rules` INT NOT NULL COMMENT '',
+  `rules` VARCHAR(200) NULL COMMENT '',
+  `support` FLOAT NULL COMMENT '',
+  `confidence` FLOAT NULL COMMENT '',
+  `lift` FLOAT NULL COMMENT '',
+  `X1` VARCHAR(45) NULL COMMENT '',
+  `X2` VARCHAR(45) NULL COMMENT '',
+  `store_id` VARCHAR(45) NULL COMMENT '',
+  PRIMARY KEY (`id_oc_rules`)  COMMENT '');

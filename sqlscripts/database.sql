a9a8310eaada7942ac58a798616984795650f11b
Eduardo Ronqui <edu.ronqui@gmail.com>
2015-11-28 22:59:28 -0200

    Changed transaction table

--- sqlscripts/database.sql
+++ sqlscripts/database.sql
@@ -1,10 +1,11 @@
-CREATE SCHEMA `oc_database` ;
+CREATE SCHEMA oc_database ;
 
-CREATE TABLE `oc_database`.`oc_transactions` (
-  `id` INT NOT NULL COMMENT '',
-  `client_id` INT NULL COMMENT '',
-  `store_id` INT NULL COMMENT '',
-  `transaction_type` VARCHAR(2) NULL COMMENT '',
-  `transaction_items` LONGTEXT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '');
+CREATE TABLE oc_database.oc_transactions (
+  id INT NOT NULL AUTO_INCREMENT ,
+  client_id INT NOT NULL,
+  transaction_id INT NOT NULL,
+  store_id INT NOT NULL,
+  transaction_type VARCHAR(2) NOT NULL,
+  item VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id));
 
